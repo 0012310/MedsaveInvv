7008e492d0603aa90923f01dd49d497db850493e
Karmveer Singh Rana <Karmveersingh2016@gmail.com>
2024-08-11 17:09:23 +0530

    After live 2.0.0.7

--- app/src/main/java/med/Save/medsaveinv/SplashActivity.kt
+++ app/src/main/java/med/Save/medsaveinv/SplashActivity.kt
@@ -4,6 +4,7 @@ package med.Save.medsaveinv
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -14,13 +15,24 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.provider.Settings
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.android.volley.AuthFailureError
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import med.Save.medsaveinv.DataBase.SharedPreferenceUtils
+import org.json.JSONObject
 
 
 @SuppressLint("CustomSplashScreen")
@@ -29,18 +41,25 @@ class SplashActivity : AppCompatActivity(), LocationListener {
     private val locationPermissionCode = 2
     private lateinit var locationManager: LocationManager
     private lateinit var sharedPreferences: SharedPreferenceUtils
-    lateinit var tvVersion: TextView
 
+    lateinit var tvVersion: TextView
+    private var appVersion = "0"
+    private var forceUpdate = "0"
+    private var latestVersion = ""
+    private var messageFromServer = ""
+    private lateinit var updateAlertDialog: androidx.appcompat.app.AlertDialog
+    private lateinit var requestQueue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         sharedPreferences = SharedPreferenceUtils.getInstance(this)
+        requestQueue = Volley.newRequestQueue(this)
         tvVersion = findViewById(R.id.tvVersion)
         val manager = this.packageManager
         val info = manager.getPackageInfo(this.packageName, PackageManager.GET_ACTIVITIES)
 
-
+        checkVersion()
         tvVersion.text = "V :"+info.versionName.toString()
 
         if (ContextCompat.checkSelfPermission(
@@ -49,12 +68,119 @@ class SplashActivity : AppCompatActivity(), LocationListener {
             ) ==
             PackageManager.PERMISSION_GRANTED
         ) {
-            waitAndGo()
+          //  waitAndGo()
         } else {
             getLocationPermission()
         }
 
 
+    }
+
+    private fun checkVersion() {
+        val manager = this.packageManager
+        val info = manager.getPackageInfo(this.packageName, PackageManager.GET_ACTIVITIES)
+        appVersion = info.versionName.toString()
+        val request: StringRequest = object : StringRequest(
+            Method.GET,
+            "https://medsave.in/AgentAPI/api/search/getAppVersion?appName=Medsaveinv",
+            Response.Listener { response ->
+                val jsonobj = JSONObject(response)
+                val jsondata = jsonobj.getJSONArray("data")
+                for (i in 0 until jsondata.length()) {
+                    var data = jsondata.getJSONObject(i)
+                    latestVersion = data.optString("CurrentVersion")
+                    forceUpdate = "1"
+                    if (latestVersion.isNotBlank()) {
+                        try {
+                            Log.d("DDDL", latestVersion)
+                            Log.d("DDDLAppVersion", appVersion)
+                            if ((latestVersion) > appVersion) {
+                                showUpdateAlertDialog()
+                            } else {
+                                waitAndGo()
+                            }
+                        } catch (ne: NumberFormatException) {
+                            ne.printStackTrace()
+                            Toast.makeText(this, "$ne", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        waitAndGo()
+                    }
+
+                }
+
+
+            },
+            Response.ErrorListener {
+                Toast.makeText(this, "Error$it", Toast.LENGTH_SHORT).show()
+            }) {
+
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers: MutableMap<String, String> = java.util.HashMap()
+                headers["X-Access-Token"] =
+                    "PZvHQLGHdMTyJBr20d3SAbU5Y2NdnW3dmRhRO6v/MfHHIsK!HrupC41?dEdgAIUR"
+                return headers
+            }
+        }
+
+        request.setShouldCache(false)
+        request.retryPolicy = DefaultRetryPolicy(
+            4000,
+            2,
+            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        )
+        requestQueue.add(request)
+    }
+
+    private fun showUpdateAlertDialog() {
+        val msg: String = if (messageFromServer.isEmpty()) {
+            getString(R.string.versionStr) + getString(R.string.versionEnd)
+        } else {
+            messageFromServer
+        }
+        updateAlertDialog = androidx.appcompat.app.AlertDialog.Builder(this)
+            .setTitle(R.string.newVersionTitle)
+            .setMessage(msg)
+            .setPositiveButton(R.string.updateBtn, null)
+            .setNegativeButton(R.string.cancelBtn, null)
+            .setCancelable(false)
+            .show()
+
+        val updateBtn = updateAlertDialog.getButton(androidx.appcompat.app.AlertDialog.BUTTON_POSITIVE) as Button
+        updateBtn.setOnClickListener {
+            updateAlertDialog.dismiss()
+            val appPackageName =
+                packageName // packageName is an extension property available in Kotlin
+            try {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        // Uri.parse("market://details?id=$appPackageName")
+                        Uri.parse("https://play.google.com/store/apps/details?id=med.Save.medsaveinv&hl=en_US")
+                    )
+                )
+                finish()
+            } catch (anfe: ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=med.Save.medsaveinv&hl=en_US")
+                    )
+                )
+            }
+        }
+
+        val cancelBtn = updateAlertDialog.getButton(androidx.appcompat.app.AlertDialog.BUTTON_NEGATIVE) as Button
+        if (forceUpdate == "1") {
+            cancelBtn.visibility = View.GONE
+        }
+        cancelBtn.setOnClickListener {
+            updateAlertDialog.dismiss()
+            waitAndGo()
+
+        }
+        updateAlertDialog.setOnKeyListener { dialog, keyCode, event -> keyCode == KeyEvent.KEYCODE_BACK }
     }
 
     private fun getLocationPermission() {
